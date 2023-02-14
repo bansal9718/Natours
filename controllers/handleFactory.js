bc06b38246052ea1db66a7262e893639fcdb9ca9
bansal9718 <rohitbansal4685@gmail.com>
2023-02-14 10:24:03 +0530

    new commit

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -64,7 +64,8 @@ exports.getAll = (Model) =>
   //to allow for nested get reviews on tour
   catchAsync(async (req, res, next) => {
     let filter = {};
-    if (req.params.tourId) filter = { tour: req.params.tourId };
+    if (req.params.tourId) 
+    filter = { tour: req.params.tourId };
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
